891398e68976c64e71e07386fb0edd73bb646726
Eveler <evelerwork@mail.ru>
2018-03-12 18:06:32 +1000

    compile.bat

--- compile.bat
+++ compile.bat
@@ -1,2 +1,2 @@
 rem C:\Python35\Scripts\nuitka.bat --standalone --recurse-all --remove-output -j 5 --windows-disable-console --plugin-enable=qt-plugins dmsic.py
-C:\Python35\Scripts\nuitka.bat --standalone --recurse-all -j 5 --plugin-enable=qt-plugins --recurse-to=sqlalchemy.sql --recurse-directory=C:\Python35\Lib\site-packages\sqlalchemy\sql\ dmsic.py
+C:\Python35\Scripts\nuitka.bat --standalone --recurse-all -j 5 dmsic.py
