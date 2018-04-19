af54cb4e9cd53155d938426c9b9d8fbae60177c6
Michael Savenko <michael.savenko@dicehub.net>
2018-04-19 18:10:51 +1000

    Fix for PyQt5 bluetooth bug on windows

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 python-3.5.4-webinstall.exe /passive InstallAllUsers=1 TargetDir=c:\Python35
-c:\Python35\python.exe -m pip install -U zeep requests lxml sqlalchemy PyQt5
+c:\Python35\python.exe -m pip install -U zeep requests lxml sqlalchemy PyQt5==5.9.2
 rem md c:\dmsic
 rem xcopy xmlsigner c:\dmsic /E /H /Y
 rem xcopy *.py c:\dmsic /E /H /Y
